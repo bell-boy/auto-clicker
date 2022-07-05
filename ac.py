4a7b1652abd0986133226ca13fc4c655f933c42c
femi <joshuaobello95@gmail.com>
2022-07-05 11:10:07 -0500

    Redo ac.py file
    Add Threading
    Add multiple times

--- ac.py
+++ ac.py
@@ -1,8 +1,22 @@
 import mouse
 import time
+import threading
+
+count = int(input("please enter a repeating count: "))
+prep_delay = int(input("please enter your prep delay: "))
 
 time.sleep(5)
 
-for x in range(10):
-	mouse.click()
-	time.sleep(1)
+def clicker(amount, freq):
+    for i in range(amount):
+        mouse.click()
+        time.sleep(amount)
+
+#TODO: OPTION FOR CUSTOM TIMES
+five = threading.Thread(target=clicker, args=(count, 5))
+five.start()
+ten = threading.Thread(target=clicker, args=(count, 10))
+ten.start()
+twenty = threading.Thread(target=clicker, args=(count, 20))
+twenty.start()
+#five = threading.Thread(target=clicker, args=(count, 5))
