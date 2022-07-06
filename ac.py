93cbf93bb9bf5b3ff60a1b5e4447972f2b7822df
femi <joshuaobello95@gmail.com>
2022-07-06 11:23:01 -0500

    Add new major features
    1. The ablility to create unlimited 'fingers' is here
    2. The quit hotkey was changed to ctrl-k due to conflicts
    3. minor bugfixes

--- ac.py
+++ ac.py
@@ -7,7 +7,7 @@ import os
 def halt():
     os._exit(0)
 
-keyboard.add_hotkey("alt+k", callback=halt)
+keyboard.add_hotkey("ctrl+k", callback=halt)
 
 count = input("please enter a repeating count: ")
 if count == "indef":
@@ -16,7 +16,8 @@ else:
 	count = int(count)
 prep_delay = int(input("please enter your prep delay: "))
 
-time.sleep(5)
+finger_count = int(input("please enter number of fingers: "))
+
 
 def clicker(amount, freq, pos_x, pos_y):
     for i in range(amount):
@@ -24,7 +25,23 @@ def clicker(amount, freq, pos_x, pos_y):
         mouse.click()
         time.sleep(freq)
 
-corner = threading.Thread(target=clicker, args=(count, 5, 946, 631))
-corner.start()
-middle = threading.Thread(target=clicker, args=(count, 5, 307, 404))
-middle.start()
+threads = list()
+
+for i in range(finger_count):
+    finger_freq = int(input("please enter finger " + str(i + 1) + " frequency: "))
+    print("click at finger location")
+    mouse.wait(button="left", target_types="down")
+    pos = mouse.get_position()   
+    x = threading.Thread(target=clicker, args=(count, finger_freq, pos[0], pos[1]))
+    threads.append(x)
+    
+time.sleep(prep_delay)
+
+for i in range(finger_count):
+    threads[i].start()
+
+
+#corner = threading.Thread(target=clicker, args=(count, 5, 946, 631))
+#corner.start()
+#middle = threading.Thread(target=clicker, args=(count, 20, 307, 404))
+#middle.start()
