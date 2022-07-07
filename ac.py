6ffc6b745d703a4bf3386cba818e4216b8a82999
femi <joshuaobello95@gmail.com>
2022-07-07 12:34:41 -0500

    Start on GUI

--- ac.py
+++ ac.py
@@ -3,12 +3,28 @@ import time
 import threading
 import keyboard
 import os
+import tkinter as tk
+
+window = tk.Tk()
+
+threads = list()
+
+stop = threading.Event()
+
+def create():
+    print('working')
 
 def halt():
-    os._exit(0)
+   stop.set()
 
 keyboard.add_hotkey("ctrl+k", callback=halt)
 
+fingers = list()
+
+create_finger = tk.Button(text="create_finger", command=create)
+create_finger.pack()
+
+"""
 count = input("please enter a repeating count: ")
 if count == "indef":
 	count = 1000000000000
@@ -19,29 +35,26 @@ prep_delay = int(input("please enter your prep delay: "))
 finger_count = int(input("please enter number of fingers: "))
 
 
-def clicker(amount, freq, pos_x, pos_y):
+def clicker(amount, freq, pos_x, pos_y, stop_event):
     for i in range(amount):
+        if stop_event.is_set():
+            break
         mouse.move(pos_x, pos_y)
         mouse.click()
         time.sleep(freq)
 
-threads = list()
 
 for i in range(finger_count):
     finger_freq = int(input("please enter finger " + str(i + 1) + " frequency: "))
     print("click at finger location")
     mouse.wait(button="left", target_types="down")
     pos = mouse.get_position()   
-    x = threading.Thread(target=clicker, args=(count, finger_freq, pos[0], pos[1]))
+    x = threading.Thread(target=clicker, args=(count, finger_freq, pos[0], pos[1], stop))
     threads.append(x)
     
 time.sleep(prep_delay)
 
 for i in range(finger_count):
     threads[i].start()
-
-
-#corner = threading.Thread(target=clicker, args=(count, 5, 946, 631))
-#corner.start()
-#middle = threading.Thread(target=clicker, args=(count, 20, 307, 404))
-#middle.start()
+"""
+window.mainloop()
