47f247d093e660441ebdb2ce08d9adbd10c576fb
femi <joshuaobello95@gmail.com>
2022-07-09 23:23:13 -0500

    Switch to floating point numbers for time

--- ac.py
+++ ac.py
@@ -4,8 +4,22 @@ import threading
 import keyboard
 import os
 
+threads = list()
+stop = threading.Event()
+
+def start(resume=False):
+    stop.set()
+    if not resume:
+        for t in range(len(threads)):
+            threads[t].start()
+
 def halt():
-    os._exit(0)
+    stop.clear()
+    next_s = input("continue? (y/n): ")
+    if next_s == "y":
+        start(resume=True)
+    else:
+        os._exit(0)
 
 keyboard.add_hotkey("ctrl+k", callback=halt)
 
@@ -14,34 +28,28 @@ if count == "indef":
 	count = 1000000000000
 else:
 	count = int(count)
-prep_delay = int(input("please enter your prep delay: "))
+prep_delay = float(input("please enter your prep delay: "))
 
-finger_count = int(input("please enter number of fingers: "))
+finger_count = float(input("please enter number of fingers: "))
 
 
-def clicker(amount, freq, pos_x, pos_y):
+def clicker(amount, freq, pos_x, pos_y, stopping_event):
     for i in range(amount):
+        stopping_event.wait()
         mouse.move(pos_x, pos_y)
         mouse.click()
         time.sleep(freq)
 
-threads = list()
 
 for i in range(finger_count):
-    finger_freq = int(input("please enter finger " + str(i + 1) + " frequency: "))
+    finger_freq = float(input("please enter finger " + str(i + 1) + " frequency: "))
     print("click at finger location")
     mouse.wait(button="left", target_types="down")
     pos = mouse.get_position()   
-    x = threading.Thread(target=clicker, args=(count, finger_freq, pos[0], pos[1]))
+    x = threading.Thread(target=clicker, args=(count, finger_freq, pos[0], pos[1], stop))
     threads.append(x)
     
 time.sleep(prep_delay)
 
-for i in range(finger_count):
-    threads[i].start()
+start()
 
-
-#corner = threading.Thread(target=clicker, args=(count, 5, 946, 631))
-#corner.start()
-#middle = threading.Thread(target=clicker, args=(count, 20, 307, 404))
-#middle.start()
